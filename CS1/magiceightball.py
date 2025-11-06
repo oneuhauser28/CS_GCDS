49110e3c4e99bd8d87241a790cac5c2e02c95381
oneuhauser28 <oneuhauser28@gcds.net>
2025-11-06 11:55:40 -0500

    Create magiceightball.py
    magic 8 ball wrong name for submission

---
+++ CS1/magiceightball.py
@@ -0,0 +1,29 @@
+
+while True:
+
+    import random
+    
+    responses = [
+        'Sure',
+        'No',
+        'Maybe',
+        'Ask again later',
+        'Yes, definitely',
+        'Do not count on it',
+        'Outlook is good',
+        'Certainly not',
+        'Absolutely'
+    ]
+    
+    user = input('Ask the Magic 8 Ball a question (press enter to quit): ')
+
+    if user == '':
+        print('Stopping the game.')
+        break
+
+    elif '?' not in user:
+        print('Please enter a valid question. ')
+        continue
+
+    else:
+        print('The Magic 8 Ball says: ', random.choice(responses))
