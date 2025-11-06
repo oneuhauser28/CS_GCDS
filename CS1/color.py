49f22dd85f411dd1415ac4244983ae4d3a901ee2
oneuhauser28 <oneuhauser28@gcds.net>
2025-11-06 11:54:49 -0500

    Update color.py
    input test

--- CS1/color.py
+++ CS1/color.py
@@ -3,7 +3,7 @@ color = "red"
 while True:
     user_color = input('Enter any color ').lower()
     if user_color == color:
-        print('Well done!')
+        print('Well done! You got it!')
         break
     else:
         print('Try again...')
