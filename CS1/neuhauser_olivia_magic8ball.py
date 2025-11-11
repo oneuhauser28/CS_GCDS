efd18ce24d68334b006abf43a7045902865baa12
oneuhauser28 <oneuhauser28@gcds.net>
2025-11-11 10:13:13 -0500

    Update neuhauser_olivia_magic8ball.py

--- CS1/neuhauser_olivia_magic8ball.py
+++ CS1/neuhauser_olivia_magic8ball.py
@@ -1,31 +1,55 @@
-while True:
+while True:                                                                             #forever loop
 
-    import random
-    import time
+    import random                                                                       #imports the random module, a built-in library for random numbers
+    import time                                                                         #imports the time module, a built-in library for time related operations
     
-    responses = [
-        'Sure',
-        'No',
-        'Maybe',
-        'Ask again later',
-        'Yes, definitely',
-        'Do not count on it',
-        'Outlook is good',
-        'Certainly not',
-        'Absolutely'
-    ]
+    responses = [                                                                       #list all possible responses
+        'Sure',                                                                 
+        'No',                                                                   
+        'Maybe',                                                                
+        'Ask again later',                                                      
+        'Yes, definitely',                                                         
+        'Do not count on it',                                                   
+        'Outlook is good',                                                      
+        'Certainly not',                                                        
+        'Absolutely',                                                           
+        'Doubtful',                                                             
+        'Undoubtedly'                                                           
+    ]                                                                                   #end list 
+
     
-    user = input('Ask the Magic 8 Ball a question (press enter to quit): ')
-    print('Shaking the 8 Ball...')
-    time.sleep(1)
+    questions = [                                                                       #list question words
+        'do',
+        'does',
+        'did',
+        'should',
+        'will',
+        'would',
+        'can',
+        'have',
+        'is',
+        'were',
+        'are',
+        'has',
+        'have',
+        'am'
+    ]                                                                                   #end list              
+    
+    user = input('Ask the Magic 8 Ball a question (press enter to quit): ').lower()     #set user as the response to the statement 'Ask the Magic 8 Ball a question (press enter to quit):, turn response lowercase
+    print('Shaking the 8 Ball...')                                                      #display 'Shaking the 8 Ball...'
+    time.sleep(1)                                                                       #delay the random response by 1 second
 
-    if user == '':
-        print('Stopping the game.')
-        break
+    if user == '':                                                                      #if user presses enter without any text
+        print('Stopping the game.')                                                     #display 'Stopping the game.'
+        break                                                                           #end loop
 
-    elif '?' not in user:
-        print('Please enter a valid question. ')
-        continue
+    elif '?' not in user:                                                               #otherwise if a question mark is not in user
+        print('Please enter a valid question. ')                                        #display 'Please enter a valid question. '
+        continue                                                                        #continue with the loop
 
-    else:
-        print('The Magic 8 Ball says: ', random.choice(responses))
+    elif not any(item in user for item in questions):                                   #if no word in the user response matches a word in the question word list
+        print('Please enter a valid question.')                                         #display 'Please enter a valid question'
+        continue                                                                        #continue with the loop 
+    
+    else:                                                                               #if none of those conditions apply
+        print('The Magic 8 Ball says: ', random.choice(responses))                      #display 'The Magic 8 Ball says:' followed by a random choice from the list of possible responses        
